b813704e58beebd938aeeabb491a4c12daa146c0
Maid Mehic <maid.mehic@compusight.com>
2020-02-28 04:11:00 -0800

    Implemented pagination

--- src/service/youtubeApi.js
+++ src/service/youtubeApi.js
@@ -3,14 +3,15 @@ import axios from 'axios'
 const ACCESS_KEY = 'AIzaSyBAbMRpszJtJCoungaMrssCX1ycD9vzyDs';
 const BASE_URL = 'https://www.googleapis.com/youtube/v3';
 
-export async function getVideosByQuery(query) {
+export async function getVideosByQuery(query, pageToken) {
     return await axios.get(BASE_URL + '/search', {
         params: {
             part: 'snippet',
             type: 'video',
-            maxResults: 5,
+            maxResults: 10,
             key: ACCESS_KEY,
-            q: query
+            q: query,
+            pageToken: pageToken
         }
     });
 }
